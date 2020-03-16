3e50d6d2d72c32188d46829acc288e98f290e536
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-03-16 22:45:11 +0200

    init + zoom

--- js/zoomer.js
+++ js/zoomer.js
@@ -1,52 +1,64 @@
 /**
  * Created by yevheniia on 16.03.20.
  */
+
+
 d3.xml("img/words.svg").then(function(data) {
 
-    d3.select("#zoom").node().append(data.documentElement);
 
-    const width = document.getElementById("zoom").getBoundingClientRect().width;
-    const height = document.getElementById("zoom").getBoundingClientRect().height;
+        d3.select("#zoom").node().append(data.documentElement);
 
-    const svg = d3.select('#zoom svg');
+        const width = document.getElementById("zoom").getBoundingClientRect().width;
+        const height = document.getElementById("zoom").getBoundingClientRect().height;
 
-    var canvas = d3.select("#zoom")
-        .append("canvas")
-        .attr("width", width)
-        .attr("height", height);
+        const svg = d3.select('#zoom svg');
 
-    var context = canvas.node().getContext("2d");
+        var canvas = d3.select("#zoom")
+            .append("canvas")
+            .attr("width", width)
+            .attr("height", height);
 
-    var DOMURL = window.URL || window.webkitURL || window;
-    var svgString = domNodeToString(svg.node());
+        var context = canvas.node().getContext("2d");
 
-    var image = new Image();
-    var svgBlob = new Blob([svgString], { type: "image/svg+xml;charset=utf-8" });
-    var url = DOMURL.createObjectURL(svgBlob);
+        var DOMURL = window.URL || window.webkitURL || window;
+        var svgString = domNodeToString(svg.node());
 
-    image.onload = function() {
-        context.drawImage(image, 0, 0);
-        DOMURL.revokeObjectURL(url);
-    };
+        var image = new Image();
+        var svgBlob = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
+        var url = DOMURL.createObjectURL(svgBlob);
 
-    image.src = url;
+        image.onload = function () {
+            context.drawImage(image, 0, 0);
+            DOMURL.revokeObjectURL(url);
+        };
+
+        image.src = url;
 
 
-    /* додаємо зум */
-    canvas.call(d3.zoom()
+        /* додаємо зум */
+        canvas.call(d3.zoom()
             .scaleExtent([1, 17])
             .on("zoom", zoom));
 
 
-    function zoom() {
-        var transform = d3.event.transform;
-        context.save();
-        context.clearRect(0, 0, width, height);
-        context.translate(transform.x, transform.y);
-        context.scale(transform.k, transform.k);
-        context.drawImage(image, 0, 0);
-        context.restore();
+        function zoom() {
+            var transform = d3.event.transform;
+            context.save();
+            context.clearRect(0, 0, width, height);
+            context.translate(transform.x, transform.y);
+            context.scale(transform.k, transform.k);
+            context.drawImage(image, 0, 0);
+            context.restore();
+        }
+
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
+
+    window.addEventListener("resize", console.log("hi"), resizeCanvas);
+
 
 
 });
